b2142530788413d5244831675dc95fce659b2f32
yangcheng <yangcheng0816@gmail.com>
2015-11-30 20:23:01 +0800

    initialize gradle

--- app/src/main/java/com/mredrock/cyxbsmobile/ui/MainActivity.java
+++ app/src/main/java/com/mredrock/cyxbsmobile/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package com.mredrock.cyxbsmobile;
+package com.mredrock.cyxbsmobile.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mredrock.cyxbsmobile.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/mredrock/cyxbsmobile/ui/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.mredrock.cyxbsmobile.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dayaa on 15/11/30.
+ */
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
