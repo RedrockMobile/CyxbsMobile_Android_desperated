64d97b87a7e6aa8e0f07e740e746e82f110a21f2
acemurder <vqqs@163.com>
2016-10-24 16:44:50 +0800

    ..

---
+++ app/src/main/java/com/mredrock/cyxbs/ui/activity/affair/WeekChooseActivity.java
@@ -0,0 +1,13 @@
+package com.mredrock.cyxbs.ui.activity.affair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeekChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week_choose);
+    }
+}
