088b0bc1217c300fba36180535f507f4acc09136
wusui <18875144597@163.com>
2017-02-22 14:06:37 +0800

    some bugs

--- app/src/main/java/com/mredrock/cyxbs/ui/fragment/lost/LostFragment.java
+++ app/src/main/java/com/mredrock/cyxbs/ui/fragment/lost/LostFragment.java
@@ -2,14 +2,18 @@ package com.mredrock.cyxbs.ui.fragment.lost;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.mredrock.cyxbs.R;
 import com.mredrock.cyxbs.ui.activity.lost.LostActivity;
 import com.mredrock.cyxbs.ui.fragment.BaseFragment;
+import com.mredrock.cyxbs.util.LogUtils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,8 +30,9 @@ public class LostFragment extends BaseFragment {
     int theme = LostActivity.THEME_LOST;
     String category;
 
-    @Bind(R.id.rv_content) RecyclerView recycler;
-
+    @Bind(R.id.information_RecyclerView) RecyclerView recycler;
+    @Bind(R.id.fab_main) FloatingActionButton mFabMain;
+    @Bind(R.id.information_refresh) SwipeRefreshLayout mSwipeRefreshLayout;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +48,8 @@ public class LostFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_lost, container, false);
+        Toast.makeText(getContext(), "show me your floating ", Toast.LENGTH_SHORT).show();
+        return inflater.inflate(R.layout.fragment_news, container, false);
     }
 
     @Override
