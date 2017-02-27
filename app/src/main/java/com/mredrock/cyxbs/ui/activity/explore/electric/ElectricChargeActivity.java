e481f65c0a1af9c6790b6759b8426a64eaa1b9d6
acemurder <vqqs@163.com>
2017-02-28 00:14:09 +0800

    finish ElectricCircleView

--- app/src/main/java/com/mredrock/cyxbs/ui/activity/explore/electric/ElectricChargeActivity.java
+++ app/src/main/java/com/mredrock/cyxbs/ui/activity/explore/electric/ElectricChargeActivity.java
@@ -3,10 +3,12 @@ package com.mredrock.cyxbs.ui.activity.explore.electric;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.jaeger.library.StatusBarUtil;
 import com.mredrock.cyxbs.APP;
 import com.mredrock.cyxbs.R;
+import com.mredrock.cyxbs.component.widget.ElectricCircleView;
 import com.mredrock.cyxbs.model.ElectricCharge;
 import com.mredrock.cyxbs.network.RequestManager;
 import com.mredrock.cyxbs.subscriber.SimpleSubscriber;
@@ -14,16 +16,26 @@ import com.mredrock.cyxbs.subscriber.SubscriberListener;
 import com.mredrock.cyxbs.util.LogUtils;
 import com.mredrock.cyxbs.util.SPUtils;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class ElectricChargeActivity extends AppCompatActivity {
     private static final String TAG = "ElectricChargeActivity";
     private String buildingNum;
     private String dormitoryNum;
+
+    @Bind(R.id.ecv_electric_circle_view)
+    ElectricCircleView electricCircleView;
+    @Bind(R.id.toolbar_title)
+    TextView mToolbarText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electric_charge);
         StatusBarUtil.setTranslucent(this, 50);
+        ButterKnife.bind(this);
+        initView();
         initData();
         LogUtils.LOGE(TAG,buildingNum + ": "+dormitoryNum);
         RequestManager.INSTANCE.queryElectricCharge(new SimpleSubscriber<ElectricCharge>(this, new SubscriberListener<ElectricCharge>() {
@@ -40,6 +52,8 @@ public class ElectricChargeActivity extends AppCompatActivity {
             @Override
             public void onNext(ElectricCharge electricCharge) {
                 super.onNext(electricCharge);
+                String  data = electricCharge.getElectricCost().get(0) + "." + electricCharge.getElectricCost().get(1);
+                electricCircleView.drawWithData(Float.parseFloat(data),data);
                 Log.d(TAG, "onNext: " + electricCharge.getRecordTime() +"   " + electricCharge.getElectricSpend());
             }
 
@@ -48,6 +62,10 @@ public class ElectricChargeActivity extends AppCompatActivity {
                 super.onStart();
             }
         }),buildingNum,dormitoryNum);
+    }
+
+    private void initView() {
+        mToolbarText.setText("查电费");
     }
 
     private void initData() {
