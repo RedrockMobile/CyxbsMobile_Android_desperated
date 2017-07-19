5883619f3d800971e456a7400f7fa399c813f303
Jay <1432562823@qq.com>
2017-07-19 12:05:17 +0800

    完成登录界面的ui更改

--- app/src/main/java/com/mredrock/cyxbs/ui/activity/LoginActivity.java
+++ app/src/main/java/com/mredrock/cyxbs/ui/activity/LoginActivity.java
@@ -3,15 +3,13 @@ package com.mredrock.cyxbs.ui.activity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatEditText;
-import android.support.v7.widget.Toolbar;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.jaeger.library.StatusBarUtil;
 import com.mredrock.cyxbs.APP;
 import com.mredrock.cyxbs.R;
-import com.mredrock.cyxbs.component.widget.TextLimitButton;
 import com.mredrock.cyxbs.event.LoginStateChangeEvent;
 import com.mredrock.cyxbs.model.User;
 import com.mredrock.cyxbs.network.RequestManager;
@@ -29,21 +27,12 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends BaseActivity {
-
-    @Bind(R.id.toolbar_title)
-    TextView toolbarTitle;
-    @Bind(R.id.toolbar)
-    Toolbar toolbar;
     @Bind(R.id.login_stu_num_edit)
     AppCompatEditText stuNumEdit;
     @Bind(R.id.login_id_num_edit)
     AppCompatEditText idNumEdit;
     @Bind(R.id.login_submit_button)
-    TextLimitButton submitButton;
-    @Bind(R.id.iv_login_account)
-    ImageView mIvLoginAccount;
-    @Bind(R.id.iv_login_password)
-    ImageView mIvLoginPassword;
+    ImageButton submitButton;
 
     public static final String TAG = "LoginActivity";
 
@@ -52,21 +41,23 @@ public class LoginActivity extends BaseActivity {
         attemptLogin();
     }
 
+    @OnClick(R.id.back)
+    void click() {
+        finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         StatusBarUtil.setTranslucent(this, 50);
         ButterKnife.bind(this);
-        initView();
         initUser();
-        iconColorChangerFn();
         autoSendFn();
-        submitButton.addTextView(stuNumEdit);
     }
 
-    /***
-     *软键盘回车登陆
+    /**
+     * 软键盘登录
      */
     private void autoSendFn() {
         idNumEdit.setOnEditorActionListener((v, actionId, event) -> {
@@ -77,42 +68,8 @@ public class LoginActivity extends BaseActivity {
         });
     }
 
-    private void iconColorChangerFn() {
-        stuNumEdit.setOnFocusChangeListener((v, hasFocus) -> {
-            if (hasFocus) {
-                mIvLoginAccount.setColorFilter(ContextCompat.getColor(this,R.color.colorAccent));
-            } else {
-                String stuNum = stuNumEdit.getText().toString();
-                if (StringUtils.isBlank(stuNum) || stuNum.length() < 10) {
-                    stuNumEdit.setError("请输入有效的学号");
-                }
-                mIvLoginAccount.setColorFilter(ContextCompat.getColor(this,R.color.gray_edit));
-            }
-        });
-        idNumEdit.setOnFocusChangeListener((v, hasFocus) -> {
-            if (hasFocus) {
-                mIvLoginPassword.setColorFilter(ContextCompat.getColor(this,R.color.colorAccent));
-            } else {
-                String idNum = idNumEdit.getText().toString();
-                mIvLoginPassword.setColorFilter(ContextCompat.getColor(this,R.color.gray_edit));
-                if (StringUtils.isBlank(idNum) || idNum.length() < 6) {
-                    idNumEdit.setError("请输入身份证后六位");
-                }
-            }
-        });
-    }
-
     private void initUser() {
         APP.setUser(this, null);
-    }
-
-    private void initView() {
-        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white);
-        toolbar.setTitle("");
-        toolbarTitle.setText("登录");
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(ContextCompat.getDrawable(this,R.drawable.back));
-        toolbar.setNavigationOnClickListener(view -> LoginActivity.this.finish());
     }
 
     public void attemptLogin() {
