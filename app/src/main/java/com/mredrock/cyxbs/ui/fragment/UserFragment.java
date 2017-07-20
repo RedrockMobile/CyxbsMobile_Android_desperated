b0329bb69e063e913d90ba18df1749cfbcf4e8df
Jay <1432562823@qq.com>
2017-07-20 14:04:11 +0800

    为我的界面的头像添加点击功能

--- app/src/main/java/com/mredrock/cyxbs/ui/fragment/UserFragment.java
+++ app/src/main/java/com/mredrock/cyxbs/ui/fragment/UserFragment.java
@@ -3,11 +3,9 @@ package com.mredrock.cyxbs.ui.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -74,7 +72,7 @@ public class UserFragment extends BaseFragment /*implements CompoundButton.OnChe
 
     private User mUser;
 
-    @OnClick({R.id.edit, R.id.name, R.id.introduce})
+    @OnClick({R.id.edit, R.id.name, R.id.introduce, R.id.avatar})
     void clickToEdit() {
         if (APP.isLogin()) {
             startActivity(new Intent(getActivity(), EditInfoActivity.class));
