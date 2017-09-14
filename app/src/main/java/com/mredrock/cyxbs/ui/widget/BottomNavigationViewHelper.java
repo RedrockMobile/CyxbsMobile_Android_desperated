9c3f1d89fc433b636a5bd91ec781843b2874c5a8
simonla <zengmingyang96@gmail.com>
2017-09-14 09:17:35 +0800

    反射失败时抛出异常

--- app/src/main/java/com/mredrock/cyxbs/ui/widget/BottomNavigationViewHelper.java
+++ app/src/main/java/com/mredrock/cyxbs/ui/widget/BottomNavigationViewHelper.java
@@ -44,7 +44,8 @@ public class BottomNavigationViewHelper {
     }
 
     private void enableBtNavAnimInternal(boolean enable) {
-        if (!enable && mMenuView != null) {
+        if (mMenuView == null) throw new RuntimeException("get mMenuView by reflect failed");
+        if (!enable) {
             try {
                 Field field1 = mMenuView.getClass().getDeclaredField("mShiftingMode");
                 field1.setAccessible(true);
@@ -59,7 +60,7 @@ public class BottomNavigationViewHelper {
     }
 
     private void updateMenuView() {
-        if (mMenuView == null) return;
+        if (mMenuView == null) throw new RuntimeException("get mMenuView by reflect failed");
         try {
             Class<?> clazz = mMenuView.getClass();
             Method method = clazz.getDeclaredMethod("updateMenuView");
@@ -78,7 +79,7 @@ public class BottomNavigationViewHelper {
     }
 
     private void setBtNavTextSizeInternal(int size) {
-        if (mMenuView == null) return;
+        if (mMenuView == null) throw new RuntimeException("get mMenuView by reflect failed");
         try {
             Field field = mMenuView.getClass().getDeclaredField("mButtons");
             field.setAccessible(true);
