ef1b4c02b0bc4370547785ad88aa5c19865b8144
Jay <1432562823@qq.com>
2017-09-17 14:34:47 +0800

    隐藏了SplashActivity的NavigationBar

--- app/src/main/java/com/mredrock/cyxbs/ui/activity/SplashActivity.java
+++ app/src/main/java/com/mredrock/cyxbs/ui/activity/SplashActivity.java
@@ -2,7 +2,9 @@ package com.mredrock.cyxbs.ui.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -44,6 +46,18 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+        decorView.setSystemUiVisibility(uiOptions);
 
         new Timer().schedule(new TimerTask() {
             @Override
